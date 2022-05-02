67e8283534afbbba3f3e84070b052490b4d993e7
Geoffrey Tsui <tsui.geoffrey@gmail.com>
2022-05-02 10:33:55 +0800

    add hardhat-deploy plugin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,16 +1,10 @@
 import 'dotenv/config';
-import { task } from 'hardhat/config';
+import 'hardhat-deploy';
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-etherscan';
 import '@nomiclabs/hardhat-waffle';
 import 'hardhat-gas-reporter';
 import 'solidity-docgen';
-
-task("accounts", "Prints the list of accounts", async (args, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(await account.address);
-  }
-});
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -25,12 +19,37 @@ module.exports = {
       }
     }
   },
+
+  networks: {
+    rinkeby: {
+      url: process.env.PROVIDER_RINKEBY,
+      chainId: 4,
+      accounts: process.env.KEYS!.split(",")
+    },
+    moonbase: {
+      url: process.env.PROVIDER_MOONBASE,
+      chainId: 1287,
+      accounts: process.env.KEYS!.split(",")
+    }
+  },
   
   gasReporter: {
     enabled: true,
-    fast: true
+    fast: true,
+    noColors: true
   },
 
+  etherscan: {
+    apiKey: {
+      mainnet: process.env.ETHERSCAN_APIKEY,
+      ropsten: process.env.ETHERSCAN_APIKEY,
+      rinkeby: process.env.ETHERSCAN_APIKEY,
+      goerli: process.env.ETHERSCAN_APIKEY,
+      kovan: process.env.ETHERSCAN_APIKEY,
+      moonbaseAlpha: process.env.MOONSCAN_APIKEY
+    }
+  },
+  
   docgen: {
     pages: 'files'
   }
